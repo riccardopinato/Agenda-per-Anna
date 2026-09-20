505f25f15013cc8e354f42292be3ea88b275cd34
riccardopinato <riccardo121296@gmail.com>
2026-09-20 12:49:42 +0200

    Initialize locale data in widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,8 +1,10 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:agenda_per_anna/main.dart';
 
 void main() {
   testWidgets('Agenda app starts', (tester) async {
+    await initializeDateFormatting('it_IT', null);
     final store = AgendaStore();
     await tester.pumpWidget(AgendaApp(store: store));
     await tester.pumpAndSettle();
