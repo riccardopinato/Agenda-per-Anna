2281d9b2055aff693f42f42c6b7954c69dcf67a1
riccardopinato <riccardo121296@gmail.com>
2026-09-22 19:33:23 +0200

    Use v0.28.0 app version

--- lib/src/agenda_store.dart
+++ lib/src/agenda_store.dart
@@ -18,7 +18,7 @@ class AgendaStore extends ChangeNotifier {
   static const _privacyGuardKey = 'privacy_guard_v1';
   static const _backupFormat = 'agenda_per_anna_backup';
   static const _backupSchemaVersion = 1;
-  static const _appVersion = '0.27.0';
+  static const _appVersion = '0.28.0';
 
   final List<AgendaItem> items = [];
   final Map<String, DayJournal> journals = {};
