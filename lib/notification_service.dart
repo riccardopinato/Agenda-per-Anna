24ecf3e18df3d973bd645cc7a984518fcf314cb2
riccardopinato <riccardo121296@gmail.com>
2026-09-22 19:35:32 +0200

    Fix notification platform detection imports

--- lib/notification_service.dart
+++ lib/notification_service.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
 
+import 'package:flutter/foundation.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_timezone/flutter_timezone.dart';
