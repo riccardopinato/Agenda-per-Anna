22dda29d6eff5a5200ff5b1adeda76a4b1893388
riccardopinato <riccardo121296@gmail.com>
2026-09-22 14:25:19 +0200

    Update reliability test for v0.22.0

--- test/reliability_test.dart
+++ test/reliability_test.dart
@@ -175,7 +175,7 @@ void main() {
     final backup =
         Map<String, dynamic>.from(jsonDecode(store.createBackupJson()) as Map);
 
-    expect(backup['appVersion'], '0.21.1');
+    expect(backup['appVersion'], '0.22.0');
 
     store.dispose();
   });
