bc25ada1c4aa3200097103ed35b3bff1db075d2b
riccardopinato <riccardo121296@gmail.com>
2026-09-23 01:30:19 +0200

    Update reliability metadata test for v0.31.0

--- test/reliability_test.dart
+++ test/reliability_test.dart
@@ -579,7 +579,7 @@ void main() {
     final backup =
         Map<String, dynamic>.from(jsonDecode(store.createBackupJson()) as Map);
 
-    expect(backup['appVersion'], '0.30.1');
+    expect(backup['appVersion'], '0.31.0');
 
     store.dispose();
   });
