e8ee9393569889c2b15c00ab2dbec77289ea56eb
riccardopinato <riccardo121296@gmail.com>
2026-09-22 19:33:26 +0200

    Register v0.28.0 push devices

--- lib/push_notification_service.dart
+++ lib/push_notification_service.dart
@@ -194,7 +194,7 @@ class PushNotificationService {
       await CloudSyncService.instance.registerPushDevice(
         token: token,
         platform: 'android',
-        appVersion: '0.27.0',
+        appVersion: '0.28.0',
       );
       _remotePushActive = true;
     } catch (_) {
