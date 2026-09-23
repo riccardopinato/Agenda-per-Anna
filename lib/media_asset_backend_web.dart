0cb137f3b1abfe5d92f1c06d407bfedd1b6a4eac
riccardopinato <riccardo121296@gmail.com>
2026-09-23 10:48:35 +0200

    Remove redundant Sembast web import

--- lib/media_asset_backend_web.dart
+++ lib/media_asset_backend_web.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 import 'dart:typed_data';
 
-import 'package:sembast/sembast.dart';
 import 'package:sembast_web/sembast_web.dart';
 
 final StoreRef<String, Map<String, Object?>> _assets =
