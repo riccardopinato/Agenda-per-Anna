9ad84b49258c0e06ffc30031bc182e30667bc6e4
riccardopinato <riccardo121296@gmail.com>
2026-09-23 10:48:21 +0200

    Fix v0.33 backup metadata test

--- test/reliability_test.dart
+++ test/reliability_test.dart
@@ -579,8 +579,9 @@ void main() {
     final store = AgendaStore();
     await store.load();
 
-    final backup =
-        Map<String, dynamic>.from(\n            jsonDecode(await store.createBackupJson()) as Map,\n          );
+    final backup = Map<String, dynamic>.from(
+      jsonDecode(await store.createBackupJson()) as Map,
+    );
 
     expect(backup['appVersion'], '0.33.0');
 
