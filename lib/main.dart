4062bb0818aaf1945758c0af19d1fd955aa4c695
riccardopinato <riccardo121296@gmail.com>
2026-09-23 02:19:05 +0200

    Wire structured local state store

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,7 @@ import 'package:uuid/uuid.dart';
 import 'backup_service.dart';
 import 'cloud_sync_service.dart';
 import 'notification_service.dart';
+import 'local_state_store.dart';
 import 'push_notification_service.dart';
 
 part 'src/app_shell.dart';
