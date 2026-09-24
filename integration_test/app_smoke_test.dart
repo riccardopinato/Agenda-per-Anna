eebe724de1813b092dc96875e07976b0fb65f30a
riccardopinato <riccardo121296@gmail.com>
2026-09-24 11:08:50 +0200

    Fix Android smoke widget imports

--- integration_test/app_smoke_test.dart
+++ integration_test/app_smoke_test.dart
@@ -1,5 +1,6 @@
 import 'dart:typed_data';
 
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:intl/date_symbol_data_local.dart';
