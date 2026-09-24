6c5a19ce5d231cae47c0d7c0fbe18d418000620b
riccardopinato <riccardo121296@gmail.com>
2026-09-24 17:25:35 +0200

    test(v0.41): import widget teardown primitive

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:agenda_per_anna/main.dart';
