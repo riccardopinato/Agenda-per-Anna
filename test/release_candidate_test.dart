97d1e7ebfb221b0d035c3435815dd75f7f3d0dfa
riccardopinato <riccardo121296@gmail.com>
2026-09-25 01:44:09 +0200

    v0.42.1: update release metadata assertions

--- test/release_candidate_test.dart
+++ test/release_candidate_test.dart
@@ -182,8 +182,8 @@ void main() {
     final appLabJourney =
         File('.maestro/applab-journey.json').readAsStringSync();
 
-    expect(appReleaseVersion, '0.42.0');
-    expect(pubspec, contains('version: 0.42.0+50'));
+    expect(appReleaseVersion, '0.42.1');
+    expect(pubspec, contains('version: 0.42.1+51'));
 
     expect(releaseWorkflow, contains('--release-signing'));
     expect(releaseWorkflow, contains('--obfuscate'));
