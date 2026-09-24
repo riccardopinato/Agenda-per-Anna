4b8d2915563de1906e320320a4e62fb84c367219
riccardopinato <riccardo121296@gmail.com>
2026-09-24 10:56:29 +0200

    Exercise persistent storage in Android integration tests

--- lib/local_state_backend_io.dart
+++ lib/local_state_backend_io.dart
@@ -4,8 +4,12 @@ import 'package:path_provider/path_provider.dart';
 import 'package:sembast/sembast_io.dart';
 import 'package:sembast/sembast_memory.dart';
 
+const bool _integrationTestPersistentStorage =
+    bool.fromEnvironment('ANNAS_DIARY_INTEGRATION_TEST');
+
 bool get localStateBackendIsTest =>
-    Platform.environment['FLUTTER_TEST'] == 'true';
+    Platform.environment['FLUTTER_TEST'] == 'true' &&
+    !_integrationTestPersistentStorage;
 
 Future<Database> openLocalStateDatabase() async {
   if (localStateBackendIsTest) {
