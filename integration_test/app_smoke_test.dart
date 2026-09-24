5028673ba91e24e66eb5e9ad9bc7ff895755622d
riccardopinato <riccardo121296@gmail.com>
2026-09-24 10:52:22 +0200

    Add Android end-to-end smoke test

---
+++ integration_test/app_smoke_test.dart
@@ -0,0 +1,55 @@
+import 'dart:typed_data';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:intl/date_symbol_data_local.dart';
+
+import 'package:agenda_per_anna/local_state_store.dart';
+import 'package:agenda_per_anna/main.dart';
+import 'package:agenda_per_anna/media_asset_store.dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  testWidgets('Android smoke: storage, media and primary navigation work',
+      (tester) async {
+    await initializeDateFormatting('it_IT', null);
+    await LocalStateStore.instance.resetForTesting();
+    await MediaAssetStore.instance.resetForTesting();
+
+    final store = AgendaStore();
+    await store.load();
+
+    await store.addInboxEntry('Smoke Android');
+    await store.setPin('2468');
+    expect(store.verifyPin('2468'), isTrue);
+
+    final mediaId = await MediaAssetStore.instance.put(
+      Uint8List.fromList([11, 22, 33, 44, 55, 66, 77, 88]),
+    );
+    expect(await MediaAssetStore.instance.read(mediaId), isNotNull);
+
+    await tester.pumpWidget(AgendaApp(store: store));
+    await tester.pumpAndSettle();
+
+    expect(find.text('Agenda per Anna'), findsOneWidget);
+    expect(find.text('Home'), findsOneWidget);
+    expect(find.text('Mese'), findsOneWidget);
+    expect(find.text('Settimana'), findsOneWidget);
+    expect(find.text('Oggi'), findsOneWidget);
+
+    await tester.tap(find.text('Mese'));
+    await tester.pumpAndSettle();
+    expect(find.text('Mese'), findsWidgets);
+
+    await tester.tap(find.text('Settimana'));
+    await tester.pumpAndSettle();
+    expect(find.text('Settimana'), findsWidgets);
+
+    await tester.tap(find.text('Oggi'));
+    await tester.pumpAndSettle();
+    expect(find.text('Oggi'), findsWidgets);
+
+    store.dispose();
+  });
+}
