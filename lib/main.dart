aad56a8b5b6f2edd671a9bede4de4993878dfcd6
riccardopinato <riccardo121296@gmail.com>
2026-09-26 10:28:58 +0200

    v0.55 wire organization domain

--- lib/main.dart
+++ lib/main.dart
@@ -33,6 +33,7 @@ part 'src/app_shell.dart';
 part 'src/domain_models.dart';
 part 'src/day_hub_domain.dart';
 part 'src/recurring_life_domain.dart';
+part 'src/organization_domain.dart';
 part 'src/people_domain.dart';
 part 'src/store_signals.dart';
 part 'src/store/backup_domain.dart';
