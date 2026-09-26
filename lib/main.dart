8b59aa1a4742809af9995ebb7528513927c20798
riccardopinato <riccardo121296@gmail.com>
2026-09-26 12:49:06 +0200

    v0.59 wire search and connections domain

--- lib/main.dart
+++ lib/main.dart
@@ -37,6 +37,7 @@ part 'src/home_widget_bridge.dart';
 part 'src/recurring_life_domain.dart';
 part 'src/organization_domain.dart';
 part 'src/people_domain.dart';
+part 'src/search_connections_domain.dart';
 part 'src/store_signals.dart';
 part 'src/store/backup_domain.dart';
 part 'src/lifecycle_domain.dart';
